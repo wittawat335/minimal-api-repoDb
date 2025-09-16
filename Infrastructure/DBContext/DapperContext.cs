e8ffc3e3d509024965c4d03e303074da173daccb
Wittawat_F <wittawat.f@softdebut.com>
2025-09-16 15:59:30 +0700

    #

---
+++ Infrastructure/DBContext/DapperContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Domain.DBContext;
+
+public class DapperContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = configuration.GetConnectionString("NorthWindDB");
+    }
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+}
--- minimal-api-repoDb/Program.cs
+++ minimal-api-repoDb/Program.cs
@@ -1,9 +1,17 @@
+using Domain.DBContext;
 using FastEndpoints;
+using FastEndpoints.Security;
 using FastEndpoints.Swagger;
 
 var bld = WebApplication.CreateBuilder();
-bld.Services.AddFastEndpoints().SwaggerDocument(); 
+bld.Services.AddFastEndpoints().SwaggerDocument();
+
+bld.Services.AddAuthenticationJwtBearer(_ => _.SigningKey = bld.Configuration["JwtSecret"]);
+bld.Services.AddAuthentication();
+
+bld.Services.AddSingleton<DapperContext>();
 
 var app = bld.Build();
 app.UseFastEndpoints().UseSwaggerGen(); 
+app.UseAuthentication().UseAuthorization();
 app.Run();
