13b7a35045e4aaf4c894710460fd00e6d8bf5c59
Wittawat_F <wittawat.f@softdebut.com>
2025-09-19 11:29:15 +0700

    install lib

---
+++ minimal-api-repoDb/Data/EntityDatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace minimal_api_repoDb.Data;
+
+public class EntityDatabaseContext : DbContext
+{
+    public EntityDatabaseContext(DbContextOptions<EntityDatabaseContext> options) : base(options)
+    {
+    }
+   
+}
--- minimal-api-repoDb/Program.cs
+++ minimal-api-repoDb/Program.cs
@@ -1,17 +1,52 @@
-using Domain.DBContext;
 using FastEndpoints;
 using FastEndpoints.Security;
 using FastEndpoints.Swagger;
+using GraphQL.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using minimal_api_repoDb.Data;
 
-var bld = WebApplication.CreateBuilder();
-bld.Services.AddFastEndpoints().SwaggerDocument();
+var builder = WebApplication.CreateBuilder(args);
 
-bld.Services.AddAuthenticationJwtBearer(_ => _.SigningKey = bld.Configuration["JwtSecret"]);
-bld.Services.AddAuthentication();
+// Add services to the container.
 
-bld.Services.AddSingleton<DapperContext>();
+builder.Services.AddControllers();
+//builder.Services.AddScoped<EmployeeRepository>();
+//builder.Services.AddScoped<EmployeeQuery>();
+//builder.Services.AddScoped<EmployeeMutation>();
+//builder.Services.AddScoped<AppSchema>();
 
-var app = bld.Build();
-app.UseFastEndpoints().UseSwaggerGen(); 
-app.UseAuthentication().UseAuthorization();
+builder.Services.AddFastEndpoints().SwaggerDocument();
+
+//GraphQL
+// register graphQL
+builder.Services.AddGraphQL().AddSystemTextJson();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EntityDatabaseContext>(options 
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeDB"), 
+    sqlServerOptions => sqlServerOptions.CommandTimeout(60)), ServiceLifetime.Transient);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+//GraphQL
+//app.UseGraphQL<AppSchema>();
+app.UseGraphQLGraphiQL("/ui/graphql");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseFastEndpoints().UseSwaggerGen();
+
 app.Run();
