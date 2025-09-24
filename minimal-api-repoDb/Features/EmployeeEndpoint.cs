fd0726aea494538c08c1743acd5e7de65060853f
Wittawat_F <wittawat.f@softdebut.com>
2025-09-24 11:29:54 +0700

    #

---
+++ minimal-api-repoDb/Features/EmployeeEndpoint.cs
@@ -0,0 +1,8 @@
+﻿using FastEndpoints;
+using minimal_api_repoDb.Data.Models;
+
+namespace minimal_api_repoDb.Features;
+
+public class EmployeeEndpoint : EndpointWithoutRequest<List<Employee>>
+{
+}
