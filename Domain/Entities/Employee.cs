5a3bab5194274fd9456860e66c6eec1973efaf32
Wittawat_F <wittawat.f@softdebut.com>
2025-09-24 17:14:11 +0700

    update repo

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
+
--- Domain/Interfaces/IDapperRepository.cs
+++ Domain/Interfaces/IDapperRepository.cs
@@ -4,8 +4,14 @@ namespace Domain.Interfaces;
 
 public interface IDapperRepository<T> where T : class
 {
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<IEnumerable<T>> GetAllAsync(Expression<Func<IQueryable<T>, IOrderedQueryable<T>>>? orderBy = null);
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, Expression<Func<IQueryable<T>, IOrderedQueryable<T>>>? orderBy = null);
+    Task<IEnumerable<TParent>> GetAllWithIncludeAsync<TParent, TChild>(
+    string childTable,
+    string parentKey,
+    string childKey,
+    Func<TParent, TChild, TParent> map,
+    string? orderBy = null);
     Task<T?> GetByIdAsync(object id);
     Task<int> InsertAsync(T entity);
     Task<int> UpdateAsync(T entity);
