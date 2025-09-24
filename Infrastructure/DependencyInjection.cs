66409ef6edad4a58d80bc86d5694b8143902bc99
Wittawat_F <wittawat.f@softdebut.com>
2025-09-24 16:14:18 +0700

    add inject

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DBContext;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection InjectInfra(this IServiceCollection services)
+    {
+        services.AddSingleton<DapperContext>();
+        return services;
+    }
+
+}
