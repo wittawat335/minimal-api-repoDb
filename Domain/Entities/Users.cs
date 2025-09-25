141da390a0a6785b0fb9880266d97bd5572b536e
Wittawat_F <wittawat.f@softdebut.com>
2025-09-25 16:24:56 +0700

    add entity

---
+++ Domain/Entities/Users.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Entities;
+
+public class Users
+{
+    public int? UserID { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string? PasswordSalt { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public int? RoleID { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsEmailVerified { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+}
