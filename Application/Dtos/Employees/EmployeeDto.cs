8479ad10b0aee106f8591aca32d4fab62906c103
Wittawat_F <wittawat.f@softdebut.com>
2025-09-26 17:02:35 +0700

    add emp dto

---
+++ Application/Dtos/Employees/EmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Dtos.Employees;
+
+public class EmployeeDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
